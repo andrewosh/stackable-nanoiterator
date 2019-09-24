8e01a9c0cd292e179ecc14c94475b98f1e7d7d9f
Andrew Osheroff <andrewosh@gmail.com>
2019-09-24 15:18:10 +0200

    First commit with working version

---
+++ index.js
@@ -0,0 +1,56 @@
+class StackIterator {
+  constructor (opts = {}) {
+    this.maxDepth = opts.maxDepth || -1
+    this.map = opts.map
+    this.onpush = opts.onpush
+    this.onpop = opts.onpop
+
+    this._stack = []
+    this._destroyed = false
+    this._depth = 0
+  }
+
+  push (iterator, state) {
+    if ((this._depth >= this.maxDepth) && this.maxDepth !== -1) return
+    this._depth++
+    const entry = { iterator, state }
+    this._stack.unshift(entry)
+    if (this.onpush) this.onpush(entry)
+  }
+
+  next (cb) {
+    if (this._destroyed) return cb(new Error('Iterator was destroyed.'))
+    if (!this._stack.length) return cb(null, null)
+    const { iterator } = this._stack[0]
+    return iterator.next((err, val) => {
+      if (err) return cb(err)
+      if (val === null) {
+        const popped = this._stack.shift()
+        this._depth--
+        if (this.onpop) this.onpop(popped)
+        return this.next(cb)
+      }
+      if (this.map) val = this.map(val, this._stack.map(s => s.state))
+      return cb(null, val)
+    })
+  }
+
+  destroy (cb) {
+    this._destroyed = true
+    var pending = this._stack.length
+    if (!pending) return process.nextTick(cb, null)
+    var error = null
+    for (const { iterator } of this._stack) {
+      iterator.destroy(err => {
+        if (err) {
+          error = err
+          pending--
+          return
+        }
+        if (!--pending) return cb(error)
+      })
+    }
+  }
+}
+
+module.exports = StackIterator
